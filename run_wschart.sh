c514839c222e8abf4ca379e6fa2371ea1a3b7069
mir94 <mir94@DESKTOP-P9N0595>
2021-05-12 14:31:40 +0900

    2

--- run_wschart.sh
+++ run_wschart.sh
@@ -2,19 +2,18 @@ export SERVICE=wschart
 export DOMAIN=${SERVICE}.ddns.net
 export IMAGE=registry.ddns.net:5000/wschart:latest
 #export NODE_ID=$(docker info -f '{{.Swarm.NodeID}}')
-path=${PWD}
-echo "path=${path}"
-cd "${PWD}/${SERVICE}"
+#path=${PWD}
+#echo "path=${path}"
+#cd "${PWD}/${SERVICE}"
 echo "${PWD}"
 
 ./mvnw clean package -DskipTests
 
 #cp ${PWD}/target/*.jar ${path}/${SERVICE}/app.jar
 
-cd ${path}
-echo "${PWD}"
-docker-compose -f ${PWD}/${SERVICE}/${SERVICE}.yml build
-docker-compose -f ${PWD}/${SERVICE}/${SERVICE}.yml push
+#cd ${path}
+docker-compose -f ./${SERVICE}.yml build
+docker-compose -f ./${SERVICE}.yml push
 
 echo "[build & push success]------------------------------------"
 
@@ -24,4 +23,4 @@ do
   echo "$(docker-machine ssh $node "docker pull ${IMAGE}")"
 done
 
-docker stack deploy -c ./${SERVICE}/${SERVICE}.yml ${SERVICE}
+docker stack deploy -c ./${SERVICE}.yml ${SERVICE}
