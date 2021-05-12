150cad25a3e7c20451b56d40bcc975253ff631ff
mir94 <mir94@DESKTOP-P9N0595>
2021-05-12 15:27:27 +0900

    22

--- run_wschart.sh
+++ run_wschart.sh
@@ -2,21 +2,20 @@ export SERVICE=wschart
 export DOMAIN=${SERVICE}.ddns.net
 export IMAGE=registry.ddns.net:5000/wschart:latest
 #export NODE_ID=$(docker info -f '{{.Swarm.NodeID}}')
-#path=${PWD}
+path=${PWD}
 #echo "path=${path}"
-#cd "${PWD}/${SERVICE}"
+cd "${PWD}/${SERVICE}"
 echo "${PWD}"
 
 ./mvnw clean package -DskipTests
 
 #cp ${PWD}/target/*.jar ${path}/${SERVICE}/app.jar
 
-#cd ${path}
+#
 docker-compose -f ./${SERVICE}.yml build
 docker-compose -f ./${SERVICE}.yml push
 
 echo "[build & push success]------------------------------------!!"
-
 
 for node in $(docker-machine ls -q --filter state=Running)
 do
@@ -25,3 +24,5 @@ do
 done
 
 docker stack deploy -c ./${SERVICE}.yml ${SERVICE}
+
+cd ${path}
