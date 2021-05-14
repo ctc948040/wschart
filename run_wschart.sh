ac141fcdef7c3d6887a4950f776f5a44387dce0d
ctc948040 <mir948040@naver.com>
2021-05-14 16:16:26 +0900

    modify file

--- run_wschart.sh
+++ run_wschart.sh
@@ -4,15 +4,12 @@ export IMAGE=registry.ddns.net:5000/wschart:latest
 #export NODE_ID=$(docker info -f '{{.Swarm.NodeID}}')
 path=${PWD}
 #echo "path=${path}"
-cd "${PWD}/${SERVICE}"
+#cd "${PWD}/${SERVICE}"
 echo "${PWD}"
 
-#./mvnw clean package -DskipTests
+./mvnw clean package -DskipTests
 
-#cp ${PWD}/target/*.jar ${path}/${SERVICE}/app.jar
-
-#
-#docker-compose -f ./${SERVICE}.yml build
+docker-compose -f ./${SERVICE}.yml build
 #docker-compose -f ./${SERVICE}.yml push
 #
 #echo "[build & push success]------------------------------------!!!"
@@ -25,4 +22,4 @@ echo "${PWD}"
 
 docker stack deploy -c ./${SERVICE}.yml ${SERVICE}
 
-cd ${path}
+#cd ${path}
